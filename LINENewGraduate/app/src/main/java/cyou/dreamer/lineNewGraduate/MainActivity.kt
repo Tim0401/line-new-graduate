317172bf4b3bd917f54c83f19ff55ac5b3561733
Tsukasa Murata <stepfootprint@gmail.com>
2021-04-02 00:43:09 +0900

    firebaseでのログイン処理実装

--- LINENewGraduate/app/src/main/java/cyou/dreamer/lineNewGraduate/MainActivity.kt
+++ LINENewGraduate/app/src/main/java/cyou/dreamer/lineNewGraduate/MainActivity.kt
@@ -1,11 +1,39 @@
 package cyou.dreamer.lineNewGraduate
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+            } else {
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (FirebaseAuth.getInstance().currentUser == null){
+            // Choose authentication providers
+            val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build(),
+                AuthUI.IdpConfig.GoogleBuilder().build(),
+            )
+            startForResult.launch(AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build())
+        }
     }
 }
